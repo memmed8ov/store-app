235d74789b93f9586824d74aa327b9e3dc8629ae
Reyhan Memmedova <memmedova8m@gmail.com>
2023-01-08 19:40:25 +0400

    +fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import { BrowserRouter as Router, Routes, Route, } from 'react-router-dom'
 import { Login } from './rout/login/login';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Button from '@mui/material/Button';
 
-
+function NavBar(){
+  return<><AppBar component='nav'>
+    <Toolbar>
+    
+    </Toolbar>
+  </AppBar>
+  </>
+}
  function App() {
 
   return <>
--- src/rout/login/login.jsx
+++ src/rout/login/login.jsx
@@ -1,18 +1,34 @@
 import axios from "axios"
 import { useState } from "react"
+import VisibilityIcon from '@mui/icons-material/Visibility';
 
 export function Login() {
     const [username, setUserName] = useState('')
     const [password, setPassword] = useState('')
+    const [show, setShow] = useState('password')
 
     return <>
-        <main style={{ background: 'rgba', width: '400px', height: '400px', margin: '100px', borderRadius: '50px', textAlign: 'center' }}><h1 style={{ paddingTop: '20px' }}>LOGIN</h1>
+        <main style={{ width: '400px', height: '400px', margin: '100px auto', borderRadius: '50px', textAlign: 'center' }}>
+            <h1 style={{ paddingTop: '20px' }}>LOGIN</h1>
             <br />
             <br />
             <input type="text" placeholder="username" style={{ height: '30px', width: '200px', borderBottom: '10px', textAlign: 'center', color: 'white' }} value={username} onChange={e => setUserName(e.target.value)} />
             <br />
             <br />
-            <input type="password" placeholder="password" style={{ height: '30px', width: '200px', borderBottom: '10px', textAlign: 'center', color: 'white' }} value={password} onChange={e => setPassword(e.target.value)} />
+            <input type={show} placeholder="password" style={{ height: '30px', width: '200px', borderBottom: '10px', textAlign: 'center', color: 'white' }} value={password} onChange={e => setPassword(e.target.value)} />
+            <button style={{
+                cursor: 'hand',
+                marginLeft: -20,
+                border: 0,
+                borderRadius: 38,
+                color: 'grey'
+
+            }}>
+                <VisibilityIcon />
+            </button>
+
+
+
             <br />
             <br />
             <button onClick={() => {
@@ -23,6 +39,9 @@ export function Login() {
                 axios.post('http://tiswork.tisserv.net:8008/authentication/token', object).then((response) => {
                     const token = response.data.token.content
                     localStorage.setItem('token', token)
+
+                }, err => {
+                    alert('username or password is incorrect')
                 })
             }} style={{ width: "70px", height: "30px", textAlign: 'center', color: 'grey' }}>sign in</button>
         </main>
